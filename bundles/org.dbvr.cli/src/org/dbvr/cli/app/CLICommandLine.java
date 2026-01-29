2a21a1ff3c7988bbfa95aefb82ac25e48a0286df
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2026-01-29 20:10:49 +0300

    dbeaver/pro#7888 interface for cli context

--- bundles/org.dbvr.cli/src/org/dbvr/cli/app/CLICommandLine.java
+++ bundles/org.dbvr.cli/src/org/dbvr/cli/app/CLICommandLine.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +20,8 @@ import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.dbeaver.model.cli.ApplicationCommandLine;
 import org.jkiss.dbeaver.model.cli.ApplicationInstanceController;
+import org.jkiss.dbeaver.model.cli.CLIContextImpl;
 import org.jkiss.dbeaver.model.cli.CLIRunMeta;
-import org.jkiss.dbeaver.model.cli.CommandLineContext;
 import org.jkiss.dbeaver.model.cli.command.AbstractTopLevelCommand;
 
 public class CLICommandLine extends ApplicationCommandLine<ApplicationInstanceController> {
@@ -32,7 +32,7 @@ public class CLICommandLine extends ApplicationCommandLine<ApplicationInstanceCo
     @Override
     protected AbstractTopLevelCommand createTopLevelCommand(
         @Nullable ApplicationInstanceController applicationInstanceController,
-        @NotNull CommandLineContext context,
+        @NotNull CLIContextImpl context,
         @NotNull CLIRunMeta runMeta
     ) {
         return new CLITopLevelCommand(applicationInstanceController, context, runMeta);
