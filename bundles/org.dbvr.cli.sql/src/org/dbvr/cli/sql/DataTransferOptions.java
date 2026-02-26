59d700f12df4f7f089c514881956393dfaf874e7
Tsimafei Labanovich <timofeilobanovich@gmail.com>
2026-02-26 14:26:56 +0100

    dbeaver/pro#8509 Remove dots from dbvr help desctiptions

--- bundles/org.dbvr.cli.sql/src/org/dbvr/cli/sql/DataTransferOptions.java
+++ bundles/org.dbvr.cli.sql/src/org/dbvr/cli/sql/DataTransferOptions.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2025 DBeaver Corp and others
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,7 +39,7 @@ public class DataTransferOptions {
     @Nullable
     @CommandLine.Option(names = {"-op", "--output-format-parameters"},
         arity = "1",
-        description = "Parameters (exporter options as list of props prop1=value1,prop2=value2 coma and space are delimiters).")
+        description = "Parameters (exporter options as list of props prop1=value1,prop2=value2 coma and space are delimiters)")
     private String outputFormatParameters;
 
 
