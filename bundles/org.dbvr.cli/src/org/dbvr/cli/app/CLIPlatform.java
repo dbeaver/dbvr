24a6f45027a554e85022fe5d8ec941d81b73bd58
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2026-03-10 19:57:02 +0300

    dbeaver/pro#7595 init truststore

--- bundles/org.dbvr.cli/src/org/dbvr/cli/app/CLIPlatform.java
+++ bundles/org.dbvr.cli/src/org/dbvr/cli/app/CLIPlatform.java
@@ -71,6 +71,7 @@ public class CLIPlatform extends BasePlatformImpl {
 
         // Register properties adapter
         try {
+            getApplication().beforeWorkspaceInitialization();
             this.workspace = getApplication().createWorkspace(this);
             this.workspace.initializeProjects();
         } catch (Exception e) {
